46472d98b93aea8d96f124117b0a523aa78da13f
HuitziDomingo <huitzitzilidomingo@gmail.com>
2022-01-10 13:35:37 -0600

    primera pagina y tema dark v1 listo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Container, Box, Heading, Image, useColorModeValue, Link, Button } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-                Huitziztili Domingo Leon Nieves.El mejor desarrollador MEXICANO MERN Stack.
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                align="center"
+            >
+                Hola soy un programador WEB MERN STACK con residencia en CDMX.
             </Box>
             <Box display={{ md: 'flex' }}>
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">Huitzitzili Maestro aveces discipulo siempre.</Heading>
+                    <p>Diseñador web/Diseñador movil/Programacion y bases de datos con Express y MongoDB.</p>
                 </Box>
                 <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6, }} align="center">
                     <Image
-                        borderColor="whiteAlpha.800"
+                        // borderColor={useColorModeValue('whiteAlpha.200', 'whiteAlpha.800')}
+                        borderColor='whiteAlpha.800'
                         borderWidth={2}
                         borderStyle="solid"
                         maxWidth="100px"
@@ -23,6 +37,45 @@ const Page = () => {
                     />
                 </Box>
             </Box>
+            <Section delay={.1}>
+                <Heading as="h3" variant="section=title" mt={6}>Acerca de mi.</Heading>
+                <Paragraph>
+                    Soy un desarrollador web, que se a disciplinado en el area frontend desde hace mas de 5 años.
+                    Mi participacion en el desarrollo web a sido ferviente desde que cree junto con unos amigos una
+                    pequeña agencia de desarrollo freelance llamada {' '}
+                    <Link href="https://www.debred.com">Debred</Link>
+                    .
+                </Paragraph>
+                <Box align="center" my={4}>
+                    <NextLink href="/works">
+                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">Mi portafolio</Button>
+                    </NextLink>
+                </Box>
+            </Section>
+            <Section delay={.3}>
+                <Heading as="h3" variant="section-tilte">
+                    Biografía
+                </Heading>
+                <BioSection>
+                    <BioYear>1993</BioYear>
+                    Nacimiemto en (CDMX) México.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2018-Ago</BioYear>
+                    Frontend Developer en Prossa
+                    Ajustes de aplicaiones web usando tecnologias como HTML, CSS, Java Script y la libreria de jQuery. Contratado por proyecto.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2019-Feb</BioYear>
+                    Frontend Developer en Deq
+                    Diseño de sitios web apartir de un mock-up en el software de AdobeXD . Uso de algunas herramientas de CSS como Bootstrap y de Java Script como jQuery Para la construccion y diseño de aplicaciones web.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2021-Feb</BioYear>
+                    Backend Developer en Anemona
+                    Colaboracion para un aplicaciones de mapas usando como lenguajes principales PHP y Java Script, asicomo algunos frameorks, tales como jQuery y Bootstrap.
+                </BioSection>
+            </Section>
         </Container>
     )
 }
