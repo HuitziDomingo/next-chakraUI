4c26892ba493d859c2de2fcc25e9b5469bc3e43e
HuitziDomingo <huitzitzilidomingo@gmail.com>
2023-05-27 11:04:42 -0600

    Contenido terminado en works solo falta imagenes

--- pages/works/aum.js
+++ pages/works/aum.js
@@ -5,37 +5,40 @@ import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 function Aum() {
-  return (
-      <Layout>
-          <Container>
-              <Title>Debred <Badge>2014</Badge> </Title>
-              <P>Un sitio web para hacer posible tu proyecto en la web.</P>
-              <P>Colaboro como programador frontend de forma Freelance.</P>
-              <List ml={4} my={4}>
-                  <ListItem>
-                      <Meta>Website</Meta>
-                      <Link href="https://www.debred.com">
-                          https://www.debred.com <ExternalLinkIcon mx="2px" />
-                      </Link>
-                  </ListItem>
-              </List>
-          </Container>
-          <WorkImage src="/img/debred.png" alt="Debred" />
-          <Container>
-              <Title>Debred <Badge>2014</Badge> </Title>
-              <P>Colaboradores Freelancers</P>
-              <List ml={4} my={4}>
-                  <ListItem>
-                      <Meta>Website</Meta>
-                      <Link href="https://www.debred.com">
-                          https://www.debred.com <ExternalLinkIcon mx="2px" />
-                      </Link>
-                  </ListItem>
-              </List>
-          </Container>
-          <WorkImage src="/img/familiaDebred.jpg" alt="familia Debred" />
-      </Layout>
-  )
+    return (
+        <Layout>
+            <Container>
+                <Title>Debred <Badge>2019</Badge> </Title>
+                <P>
+                    Un sitio web informativo de una asociacion espiritual y filosofica, basada en vegeterianismo
+                    y cultura.
+                </P>
+
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://www.auminternacional.com">
+                            http://auminternacional.org/ <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
+            </Container>
+            <WorkImage src="/img/debred.png" alt="Debred" />
+            <Container>
+                <Title>Debred <Badge>2019</Badge> </Title>
+                <P>Colaboro como programador frontend de forma Freelance.</P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://www.auminternacional.com">
+                            http://auminternacional.org/ <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
+            </Container>
+            <WorkImage src="/img/familiaDebred.jpg" alt="familia Debred" />
+        </Layout>
+    )
 }
 
 export default Aum
--- pages/works/dalyza.js
+++ pages/works/dalyza.js
@@ -5,37 +5,40 @@ import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 function Dalyza() {
-  return (
-      <Layout>
-          <Container>
-              <Title>Debred <Badge>2014</Badge> </Title>
-              <P>Un sitio web para hacer posible tu proyecto en la web.</P>
-              <P>Colaboro como programador frontend de forma Freelance.</P>
-              <List ml={4} my={4}>
-                  <ListItem>
-                      <Meta>Website</Meta>
-                      <Link href="https://www.debred.com">
-                          https://www.debred.com <ExternalLinkIcon mx="2px" />
-                      </Link>
-                  </ListItem>
-              </List>
-          </Container>
-          <WorkImage src="/img/debred.png" alt="Debred" />
-          <Container>
-              <Title>Debred <Badge>2014</Badge> </Title>
-              <P>Colaboradores Freelancers</P>
-              <List ml={4} my={4}>
-                  <ListItem>
-                      <Meta>Website</Meta>
-                      <Link href="https://www.debred.com">
-                          https://www.debred.com <ExternalLinkIcon mx="2px" />
-                      </Link>
-                  </ListItem>
-              </List>
-          </Container>
-          <WorkImage src="/img/familiaDebred.jpg" alt="familia Debred" />
-      </Layout>
-  )
+    return (
+        <Layout>
+            <Container>
+                <Title>Debred <Badge>2016</Badge> </Title>
+                <P>
+                    Un sitio web con su landing page, para vender servicios de labado de alfombras, venta de percianas y pisos
+                    entre otras cosas del nicho.
+                </P>
+
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://www.dalyza.com">
+                            https://www.dalyza.com <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
+            </Container>
+            <WorkImage src="/img/debred.png" alt="Debred" />
+            <Container>
+                <Title>Debred <Badge>2014</Badge> </Title>
+                <P>Colaboro como programador frontend de forma Freelance.</P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://www.dalyza.com">
+                            https://www.dalyza.com <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
+            </Container>
+            <WorkImage src="/img/familiaDebred.jpg" alt="familia Debred" />
+        </Layout>
+    )
 }
 
 export default Dalyza
